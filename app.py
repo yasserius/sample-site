712f53f1698677b4d35c9cf41f18b7b6a89bf442
unknown <yasserius@gmail.com>
2023-02-22 06:07:51 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_from_directory, render_template
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+    
+@app.route('/static/<path:path>')
+def send_report(path):
+    return send_from_directory('static', path)
+
+
+if __name__ == "__main__":
+    app.run()
