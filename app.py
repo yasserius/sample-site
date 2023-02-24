11fd07833ab724bd048027fed6ed5281785e29e2
unknown <yasserius@gmail.com>
2023-02-24 12:59:39 +0600

    added simple hash endpoint

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, request
+from sqlalchemy import create_engine
+from datetime import datetime
+import json
 
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+def get_db_connection():
+    server_ip   = "172.105.26.77"
+    server_port = "3306"
+    username    = "root"
+    password    = "bkdufhsifh83724892349"
+    database_name = 'elemental_analytics'
     
-@app.route('/static/<path:path>')
-def send_report(path):
-    return send_from_directory('static', path)
+    return create_engine("mysql+pymysql://"+username+":"+password+"@"+server_ip+":"+server_port+"/"+database_name, pool_pre_ping=True, pool_recycle=10)
+
+def insert_api_data(data, source='sh'):
+    ts = int(datetime.now().timestamp())
+    engine = get_db_connection()
+    with engine.connect() as con:
+        r = con.execute(
+            """
+            INSERT INTO api_data 
+            (data, timestamp, source) 
+            VALUES (%s, %s, %s) 
+            """,
+            (data, ts, source)
+        )
+
+@app.route('/api/simple_hash_bsiugwiuevfishlg', methods=['POST'])
+def simple_hash_endpoint():
+    data = request.json
+    print('simple hash metadata', data)
+    insert_api_data(json.dumps(data))
+    return {'ok': True}
+
 
 
 if __name__ == "__main__":
